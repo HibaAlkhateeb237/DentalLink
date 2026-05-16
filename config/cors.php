20d02fecfc9a197829520e2a4c55fa16d7529b65
hiba alkhateeb <hibboalk@gmail.com>
2026-05-16 06:04:58 -0900

    feat: Add CORS configuration and middleware to handle cross-origin requests

---
+++ config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'paths' => [
+        'api/*',
+        'storage/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [],
+
+    'allowed_origins_patterns' => [
+        '#^http://localhost(:\d+)?$#',
+        '#^http://127\.0\.0\.1(:\d+)?$#',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
