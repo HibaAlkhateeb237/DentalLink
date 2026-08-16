b42337812c720527ad080069c6c2ba459823049d
safa10354-4 <safaalshnouan24636@gmail.com>
2026-08-16 14:19:30 +0300

    Weekly backup for my project to Google Drive.

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('backup:run')
+    ->weeklyOn(0, '14:00')
+    ->timezone('Asia/Damascus')
+    ->name('weekly-backup')
+    ->withoutOverlapping()
+    ->onOneServer();
